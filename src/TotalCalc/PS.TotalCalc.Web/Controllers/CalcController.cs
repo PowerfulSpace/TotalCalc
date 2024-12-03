0dbc149bee971ed6001a3d64b9979cf11a873483
Alexander <sany199574@gmail.com>
2024-12-03 11:35:37 +0300

    Добавлен работающий калькулятор на стороне клиента

---
+++ src/TotalCalc/PS.TotalCalc.Web/Controllers/CalcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.TotalCalc.Web.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
