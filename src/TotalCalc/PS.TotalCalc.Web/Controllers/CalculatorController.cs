a716ecbcb42399c98423a8649879e131e5c99aff
Alexander <sany199574@gmail.com>
2024-12-03 11:45:07 +0300

    Переписал предсталвление

---
+++ src/TotalCalc/PS.TotalCalc.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PS.TotalCalc.Web.Models;
+
+namespace PS.TotalCalc.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CalculatorViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(CalculatorViewModel model)
+        {
+            if (model.FirstNumber == null || model.SecondNumber == null || string.IsNullOrEmpty(model.Operator))
+            {
+                ModelState.AddModelError("", "Введите все данные корректно.");
+                return View("Index", model);
+            }
+
+            switch (model.Operator)
+            {
+                case "+":
+                    model.Result = model.FirstNumber + model.SecondNumber;
+                    break;
+                case "-":
+                    model.Result = model.FirstNumber - model.SecondNumber;
+                    break;
+                case "*":
+                    model.Result = model.FirstNumber * model.SecondNumber;
+                    break;
+                case "/":
+                    if (model.SecondNumber == 0)
+                    {
+                        ModelState.AddModelError("", "Деление на ноль невозможно.");
+                    }
+                    else
+                    {
+                        model.Result = model.FirstNumber / model.SecondNumber;
+                    }
+                    break;
+                default:
+                    ModelState.AddModelError("", "Неверный оператор.");
+                    break;
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ src/TotalCalc/PS.TotalCalc.Web/Models/CalculatorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PS.TotalCalc.Web.Models
+{
+    public class CalculatorViewModel
+    {
+        [Display(Name = "Первое число")]
+        public double? FirstNumber { get; set; }
+
+        [Display(Name = "Второе число")]
+        public double? SecondNumber { get; set; }
+
+        [Display(Name = "Оператор")]
+        public string Operator { get; set; } = null!;
+
+        [Display(Name = "Результат")]
+        public double? Result { get; set; }
+    }
+}
